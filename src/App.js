84f3b8787972c038411e43addde019dba6f80205
Satina02 <talaibekova_s@iuca.kg>
2021-12-28 16:13:46 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter/Counter';
+import UserGreet from './components/UserGreet/UserGreet';
+import SideBar from './components/SideBar/SideBar';
+
+function App() {
+  return (
+    <div className="content">
+    <SideBar place='left' />
+    <SideBar place='right' />
+    </div>
+  );
+}
+
+export default App; 
